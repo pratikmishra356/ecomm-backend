0a2506b329f675c89a7d1a6a94f54cd3da8449aa
Pratik Mishra <pratikmishra@Pratiks-MacBook-Air.local>
2021-02-03 18:22:00 +0530

    changing settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -130,7 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
